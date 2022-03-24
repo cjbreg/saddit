d1959d51c5245fc3ad582d049a7b53a7a9f61155
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-03-24 11:30:53 +0100

    fix: hotfixing docker-compose

--- src/Services/authorization-service/src/app.module.ts
+++ src/Services/authorization-service/src/app.module.ts
@@ -1,22 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql',
-      port: 3306,
-      username: 'root',
-      password: 'root123',
-      database: 'user',
-      entities: [],
-      synchronize: true,
-      autoLoadEntities: true,
-    }),
-  ],
+  imports: [],
   controllers: [],
   providers: [],
 })
--- src/Services/user-service/src/app.module.ts
+++ src/Services/user-service/src/app.module.ts
@@ -1,23 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    UserModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql',
-      port: 3306,
-      username: 'root',
-      password: 'root123',
-      database: 'user',
-      entities: [User],
-      synchronize: true,
-      autoLoadEntities: true,
-    }),
-  ],
+  imports: [],
   controllers: [],
   providers: [],
 })
