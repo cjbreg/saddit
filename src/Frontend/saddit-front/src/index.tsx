ed9c13229047c584f7e8e58b0fa4a37164c0fc14
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-03-31 09:41:37 +0200

    feat: added auth provider

--- src/Frontend/saddit-front/src/index.tsx
+++ src/Frontend/saddit-front/src/index.tsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ""}
+    clientId={process.env.REACT_APP_AUTHO_CLIENT_ID ?? ""}
+    redirectUri={window.location.origin}
+  >
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Auth0Provider>,
   rootElement
 );
