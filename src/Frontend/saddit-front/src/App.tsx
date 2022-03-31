8b1b2379e937fbcde194683d7810b80951c169da
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-03-31 09:50:35 +0200

    feat: added router info

--- src/Frontend/saddit-front/src/App.tsx
+++ src/Frontend/saddit-front/src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./views/Home";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/Frontend/saddit-front/src/index.tsx
+++ src/Frontend/saddit-front/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
@@ -12,9 +13,11 @@ ReactDOM.render(
     clientId={process.env.REACT_APP_AUTHO_CLIENT_ID ?? ""}
     redirectUri={window.location.origin}
   >
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </Auth0Provider>,
   rootElement
 );
