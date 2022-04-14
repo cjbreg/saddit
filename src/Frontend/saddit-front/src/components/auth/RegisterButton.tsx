00eccb07df44e9d4350e3c07e1779d2b0f2b2689
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-04-14 15:00:05 +0200

    feat: init register button and form

---
+++ src/Frontend/saddit-front/src/components/auth/RegisterButton.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+import LoginForm from "./LoginForm";
+import RegisteForm from "./RegisterForm";
+
+const RegisterButton = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button colorScheme={"teal"} onClick={onOpen}>
+        Register
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Register</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <RegisteForm closeForm={onClose} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="gray" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default RegisterButton;
---
+++ src/Frontend/saddit-front/src/components/auth/RegisterForm.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import {
+  Flex,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  Box,
+  FormControl,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { auth } from "./auth.service";
+import { useDispatch } from "react-redux";
+import { fetchAccessToken, registerUser, signIn } from "../../store/actions";
+
+type Props = {
+  closeForm(): void;
+};
+
+const RegisteForm = ({ closeForm }: Props) => {
+  const dispatch = useDispatch();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmailChange = (event: any) => setEmail(event.target.value);
+  const handlePasswordChange = (event: any) => setPassword(event.target.value);
+
+  const handleRegister = () => {
+    console.log("boi");
+
+    auth.createUserWithEmailAndPassword(email, password).then((data) => {
+      dispatch(
+        registerUser({
+          email: data.user?.email,
+          username: "testUsername",
+          uid: data.user?.uid,
+        })
+      );
+      closeForm();
+    });
+  };
+
+  const accessToken = async () => {
+    const token = await auth.currentUser?.getIdToken();
+    console.log(token); // TODO: Store bearer token
+    // dispatch(fetchAccessToken({ token }));
+  };
+
+  return (
+    <Flex
+      flexDirection="column"
+      backgroundColor="white"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box minW={{ base: "70%", md: "368px" }}>
+          <form>
+            <Stack spacing={4} p="1rem">
+              <FormControl>
+                <InputGroup>
+                  <Input
+                    value={email}
+                    type="email"
+                    placeholder="email address"
+                    onChange={handleEmailChange}
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                      {showPassword ? "Hide" : "Show"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Button
+                borderRadius={0}
+                variant="solid"
+                colorScheme="teal"
+                width="full"
+                onClick={handleRegister}
+              >
+                Register
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default RegisteForm;
