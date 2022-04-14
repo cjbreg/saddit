d0661b2a7f82d4bf091c9e3492c7bc380ed28d81
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-04-14 15:00:34 +0200

    feat: init api call to servcie

---
+++ src/Frontend/saddit-front/src/shared/saddit-api-client/src/user/userClient.ts
@@ -0,0 +1,40 @@
+import { AxiosResponse } from "axios";
+import HttpService from "../httpService";
+
+interface UserRequests {
+  registerUser(
+    email: string,
+    username: string,
+    uid: string
+  ): Promise<AxiosResponse<any>>;
+  registerUser(
+    email: string,
+    username: string,
+    uid: string,
+    id: number
+  ): Promise<AxiosResponse<any>>;
+  fetchUsers(): Promise<AxiosResponse<any>>;
+}
+class UserClient extends HttpService implements UserRequests {
+  registerUser = (
+    email: string,
+    username: string,
+    uid: string
+  ): Promise<AxiosResponse<any>> => {
+    return this.post("user", { email, username, uid });
+  };
+
+  updateUser = (
+    email: string,
+    username: string,
+    uid: string,
+    id: number
+  ): Promise<AxiosResponse<any>> => {
+    return this.post("user", { email, username, uid, id });
+  };
+
+  fetchUsers = (): Promise<AxiosResponse<any, any>> => {
+    return this.get("user");
+  };
+}
+export default UserClient;
