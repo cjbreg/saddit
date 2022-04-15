86c407d8604cc24d4e63f1bc3cb762527fad7705
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-04-15 11:08:26 +0200

    fix: cors error in development

--- src/Services/gateway/src/main.ts
+++ src/Services/gateway/src/main.ts
@@ -5,6 +5,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
