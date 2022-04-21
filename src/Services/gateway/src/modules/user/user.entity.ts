622610c88de5662e51ff116a5411bda6738a7e67
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-04-21 11:22:24 +0200

    feat: added validation to user model

--- src/Services/gateway/src/modules/user/user.entity.ts
+++ src/Services/gateway/src/modules/user/user.entity.ts
@@ -1,13 +1,30 @@
-import { IsEmail, IsNumberString } from 'class-validator';
+import {
+  IsBoolean,
+  IsDate,
+  IsEmail,
+  IsNumberString,
+  IsString,
+} from 'class-validator';
 
 export class User {
-  @IsNumberString() // TODO: implement validator
+  @IsNumberString()
   id: number;
 
+  @IsString()
   username: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
   uid: string;
+
+  @IsDate()
   createdAt: Date;
+
+  @IsDate()
   updatedAt: Date;
+
+  @IsBoolean()
   isActive: boolean;
 }
