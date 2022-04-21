e1b61f219b0b951f4704624a9adfeebbbf2a0c5f
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-04-21 12:13:06 +0200

    feat: added basic test

---
+++ src/Services/user-service/src/modules/user/user.controller.spec.ts
@@ -0,0 +1,49 @@
+import { ConfigModule } from '@nestjs/config';
+import { Test } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user.controller';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+
+describe('UserController', () => {
+  let userController: UserController;
+  let userService: UserService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forFeature([User]),
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.mysql_host,
+          port: 3306,
+          username: process.env.mysql_username,
+          password: process.env.mysql_password,
+          database: process.env.mysql_database,
+          entities: [User],
+          synchronize: true,
+          autoLoadEntities: true,
+        }),
+      ],
+      controllers: [UserController],
+      providers: [UserService],
+    }).compile();
+
+    userService = moduleRef.get<UserService>(UserService);
+    userController = moduleRef.get<UserController>(UserController);
+  });
+
+  describe('findAll', () => {
+    it('should return array of users', async () => {
+      const result = [];
+      jest
+        .spyOn(userService, 'findAll')
+        .mockImplementation(
+          () => new Promise((resolve, reject) => resolve(result)),
+        );
+
+      expect(await userController.findAll()).toBe(result);
+    });
+  });
+});
