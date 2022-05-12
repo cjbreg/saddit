f43d4fe617816fd5815a3326431b752e0dfd4013
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-05-12 09:52:23 +0200

    feat; added first tesst

---
+++ src/Services/gateway/src/app.controller.spec.ts
@@ -0,0 +1,8 @@
+describe('Controller', () => {
+  describe('First Test', () => {
+    it('Test should run', async () => {
+      const testData = 'Test runs';
+      expect(testData).toBe('Test runs');
+    });
+  });
+});
