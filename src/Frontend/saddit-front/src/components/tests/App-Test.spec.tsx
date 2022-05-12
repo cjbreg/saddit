16d1c7c9ebb04a3d698c98548ca1b288c9d8a299
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-05-12 10:44:10 +0200

    feat: added test to frontend

---
+++ src/Frontend/saddit-front/src/components/tests/App-Test.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { App } from "./App-Test";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText("app");
+  expect(linkElement).toBeInTheDocument();
+});
