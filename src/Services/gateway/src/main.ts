6222238ef3f921ab6557d70cb0e7e344848c0502
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-05-30 17:12:46 +0200

    fix: cors error

--- src/Services/gateway/src/main.ts
+++ src/Services/gateway/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://20.54.89.32',
     credentials: true,
   });
 
