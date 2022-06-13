5715899c47f401d02a22cbb6fa3092753ae2728e
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-06-13 15:55:11 +0200

    feat: updated test.js

--- k6/test.js
+++ k6/test.js
@@ -4,26 +4,26 @@ import { group, check, sleep } from "k6";
 export const options = {
   thresholds: {
     http_req_failed: ["rate<0.01"], // http errors should be less than 1%
-    http_req_duration: ["p(95)<500"], // 95 percent of response times must be below 500ms
+    http_req_duration: ["p(95)<200"], // 95 percent of response times must be below 200ms
   },
   scenarios: {
     user_service: {
       exec: "AllUsersTest",
       executor: "ramping-vus",
-      startVUs: 100,
+      startVUs: 0,
       stages: [
-        { duration: "60s", target: 200 },
-        { duration: "60s", target: 200 },
+        { duration: "60s", target: 500 },
+        { duration: "60s", target: 500 },
         { duration: "60s", target: 0 },
       ],
     },
     post_service: {
       exec: "AllPostsTest",
       executor: "ramping-vus",
-      startVUs: 100,
+      startVUs: 0,
       stages: [
-        { duration: "60s", target: 200 },
-        { duration: "60s", target: 200 },
+        { duration: "60s", target: 500 },
+        { duration: "60s", target: 500 },
         { duration: "60s", target: 0 },
       ],
     },
