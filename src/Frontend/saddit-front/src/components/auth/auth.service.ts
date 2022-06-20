ec8122fa659bc6ef08b0c80e5d648d2eb05e0e5d
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-06-20 12:35:13 +0200

    fix: possible .env fix

--- src/Frontend/saddit-front/src/components/auth/auth.service.ts
+++ src/Frontend/saddit-front/src/components/auth/auth.service.ts
@@ -2,14 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  databaseURL: process.env.REACT_APP_databaseURL,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
+  apiKey: String(process.env.REACT_APP_apiKey),
+  authDomain: String(process.env.REACT_APP_authDomain),
+  databaseURL: String(process.env.REACT_APP_databaseURL),
+  projectId: String(process.env.REACT_APP_projectId),
+  storageBucket: String(process.env.REACT_APP_storageBucket),
+  messagingSenderId: String(process.env.REACT_APP_messagingSenderId),
+  appId: String(process.env.REACT_APP_appId),
+  measurementId: String(process.env.REACT_APP_measurementId),
 };
 
 firebase.initializeApp(firebaseConfig);
--- src/Frontend/saddit-front/src/views/SubSadditView.tsx
+++ src/Frontend/saddit-front/src/views/SubSadditView.tsx
@@ -13,6 +13,8 @@ const SubSadditView = () => {
 
   useEffect(() => {
     requestPosts();
+    console.log(process.env);
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [subSadditName]);
 
